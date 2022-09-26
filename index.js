af614eddbe967814f4fb59bddfb1928ec8de3608
younghyo <sonata8756@gmail.com>
2022-09-26 23:17:34 +0900

    changed to a blinking rectangle

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 console.log('hello world!');
 
-function draw() {
+function draw(color) {
     const canvas = document.getElementById('canvas1');
     if (canvas.getContext) {
         const ctx = canvas.getContext('2d');
 
+        ctx.fillStyle = color;
         ctx.fillRect(25, 25, 100, 100);
         ctx.clearRect(45, 45, 60, 60);
         ctx.strokeRect(50, 50, 50, 50);
     }
 }
-draw();
+// draw();
 
+setInterval(onGameLoop, 200); // 33 milliseconds = ~ 30 frames per sec
+
+let cnt = 0;
+function onGameLoop() {
+    console.log('tick!');
+    if (cnt % 2 == 0) {
+        draw('green');
+    }
+    else {
+        draw('red');
+    }
+    cnt++;
+}
